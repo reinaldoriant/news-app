8670db4f7969e904e0765ff703cebb6ff8596db1
Reinaldo <reinaldo.riant@kompas.com>
2021-07-20 21:53:27 +0700

    + add news response + add article view

--- News App/Model/NewsResponse.swift
+++ News App/Model/NewsResponse.swift
@@ -16,14 +16,27 @@ struct NewsResponse: Codable {
 struct Article: Codable, Identifiable {
     let id = UUID()
     let author: String?
-    let url: String
+    let url: String?
     let source, title, articleDescription: String
-    let image: String
-    let date: Date
+    let image: String?
+    let date: Date?
 
     enum CodingKeys: String, CodingKey {
         case author, url, source, title
         case articleDescription = "description"
         case image, date
+    }
+}
+
+extension Article {
+    static var dummyData: Article {
+        .init(
+            author: "Kenneth Garger",
+            url: "https://nypost.com/2021/07/19/provincetown-issues-face-mask-advisory-after-new-covid-outbreak/",
+            source: "New York Post",
+            title: "Summer hotspot Provincetown issues face mask advisory after new COVID outbreak - New York Post ",
+            articleDescription: "A coronavirus outbreak linked to a Cape Cod summer vacation hotspot prompted local health officials on Monday to institute a new mask advisory, a report said.",
+            image: "https://nypost.com/wp-content/uploads/sites/2/2021/07/provincetown-mask-covid-729.jpg?quality=90&strip=all&w=1200",
+            date: "2021-07-20T03:55:00Z"
     }
 }
---
+++ News App/Views/ArticleView.swift
@@ -0,0 +1,26 @@
+//
+//  ArticleView.swift
+//  News App
+//
+//  Created by TI Digital on 20/07/21.
+//
+
+import SwiftUI
+import URLImage
+
+struct ArticleView: View {
+    
+    let article: Article
+    
+    var body: some View {
+        HStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
